875bebd33ecebfbfcb37a082b199dbd1d26da523
SamuelMiddendorp <31695207+SamuelMiddendorp@users.noreply.github.com>
2022-10-06 13:10:00 +0200

    Project setup
    LWLGL 3 with example app

---
+++ Saisho/src/main/java/com/SaishoStudios/Saisho/Core/Graphics/Renderer.java
@@ -0,0 +1,7 @@
+package com.SaishoStudios.Saisho.Core.Graphics;
+
+public class Renderer {
+    public void renderQuad(){
+
+    }
+}
---
+++ Saisho/src/main/java/com/SaishoStudios/Saisho/Core/Utils/Maths.java
@@ -0,0 +1,4 @@
+package com.SaishoStudios.Saisho.Core.Utils;
+
+public class Maths {
+}
